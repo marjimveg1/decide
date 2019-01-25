a8184d3b31900ae30445080918aed8a55c674561
marjimveg1 <maria-2696@hotmail.com>
2019-01-25 15:47:48 +0100

    Pagina inicio: #3

--- decide/visualizer/urls.py
+++ decide/visualizer/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import VisualizerView
+from .views import VisualizerView, VisualizerIndex
 
 
 urlpatterns = [
-    path('<int:voting_id>/', VisualizerView.as_view()),
+    path('inicio/', VisualizerIndex.as_view()),
+    path('display/<int:voting_id>/', VisualizerView.as_view()),
 ]
--- decide/visualizer/views.py
+++ decide/visualizer/views.py
@@ -1,8 +1,17 @@
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.http import Http404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from base import mods
+
+class VisualizerIndex(TemplateView):
+    template_name = 'visualizer/visualizerIndex.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
 
 
 class VisualizerView(TemplateView):
