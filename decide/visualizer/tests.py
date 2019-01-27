6373ef3ac869826d686f3bdb7ea8ae0c6171ddff
marjimveg1 <maria-2696@hotmail.com>
2019-01-27 20:40:56 +0100

    Implementando test

--- decide/visualizer/tests.py
+++ decide/visualizer/tests.py
@@ -11,16 +11,17 @@ from visualizer.views import Estadisticas
 class VisualizerTest(BaseTestCase):
 
     def test1(self):
-        self.question = Question(desc='pregunta1', voting=self.voting)
-        self.question.save()
+
         self.voting = Voting(pk=1000,
                              desc='descripcionVotacion1',
                              name='votacion1',
                              start_date=timezone.now(),
                              )
-
-
         self.voting.save()
+        self.question = Question(desc='pregunta1', voting=self.voting)
+        self.question.save()
+
+
         resultados = Estadisticas.getEstadisticas()
         self.assertEqual(resultados[0] ==1)
 
