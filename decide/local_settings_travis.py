36cf6da2b4c4bd171845bc3868c02ac258be774c
María Jiménez Vega <36474784+marjimveg1@users.noreply.github.com>
2019-01-27 13:08:31 +0100

    Update local_settings_travis.py

--- decide/local_settings_travis.py
+++ decide/local_settings_travis.py
@@ -1,17 +1,29 @@
 ALLOWED_HOSTS = ["*"]
 
+
 MODULES = [
+    'authentication',
     'base',
+    'booth',
+    'census',
+    'mixnet',
+    'postproc',
+    'store',
     'visualizer',
     'voting',
 ]
 
 APIS = {
+    'authentication': 'http://localhost:8000',
     'base': 'http://localhost:8000',
+    'booth': 'http://localhost:8000',
+    'census': 'http://localhost:8000',
+    'mixnet': 'http://localhost:8000',
+    'postproc': 'http://localhost:8000',
+    'store': 'http://localhost:8000',
     'visualizer': 'http://localhost:8000',
     'voting': 'http://localhost:8000',
 }
-
 BASEURL = 'http://localhost:8000'
 
 DATABASES = {
