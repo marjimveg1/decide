77d9f9abf8abb4575678b77e1376efce77e4f137
marjimveg1 <maria-2696@hotmail.com>
2019-01-27 11:28:25 +0100

    Update test

--- decide/visualizer/tests.py
+++ decide/visualizer/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from base.tests import BaseTestCase
+
+
+class VisualizerTest(BaseTestCase):
+
+    def setUp(self):
+        a = 2
+        b = 3
+        c = a + b
+
+    def setUp2(self):
+        a = 2
+        b = 3
+        c = a + b
+
+
