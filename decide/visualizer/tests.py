af43e8f94ad72ef5f694bbb4fd5d63b1ec8dddc3
marjimveg1 <maria-2696@hotmail.com>
2019-01-27 20:35:46 +0100

    Implementando test

--- decide/visualizer/tests.py
+++ decide/visualizer/tests.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 # Create your tests here.
 
 from base.tests import BaseTestCase
-from voting.models import Voting
+from voting.models import Voting, Question
 from django.utils import timezone
 from visualizer.views import Estadisticas
 
@@ -11,10 +11,15 @@ from visualizer.views import Estadisticas
 class VisualizerTest(BaseTestCase):
 
     def test1(self):
+        self.question = Question(desc='pregunta1', voting=self.voting)
+        self.question.save()
         self.voting = Voting(pk=1000,
+                             desc='descripcionVotacion1',
                              name='votacion1',
                              start_date=timezone.now(),
                              )
+
+
         self.voting.save()
         resultados = Estadisticas.getEstadisticas()
         self.assertEqual(resultados[0] ==1)
