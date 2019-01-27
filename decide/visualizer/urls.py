4f8e38444dd2b9384968f94443196376fcbd9149
julgargal <juliagarciagallego@hotmail.com>
2019-01-27 09:30:14 +0100

    implementacion bootstrap votaciones y vistas de votacion #6

--- decide/visualizer/urls.py
+++ decide/visualizer/urls.py
@@ -3,7 +3,7 @@ from .views import VisualizerView, VisualizerIndex, VisualizerTodasVotaciones
 
 
 urlpatterns = [
-    path('inicio/', VisualizerIndex.as_view()),
-    path('todasVotaciones/', VisualizerTodasVotaciones.as_view()),
+    path('sobreNosotros/', VisualizerIndex.as_view()),
+    path('votaciones/', VisualizerTodasVotaciones.as_view()),
     path('mostrar/<int:voting_id>/', VisualizerView.as_view()),
 ]
