e118c84218374439c3b16a29cefe5cce52348757
marjimveg1 <maria-2696@hotmail.com>
2019-01-28 12:44:28 +0100

    Añadido estadistica votaciones mensuales #10

--- decide/visualizer/tests.py
+++ decide/visualizer/tests.py
@@ -6,24 +6,90 @@ from base.tests import BaseTestCase
 from voting.models import Voting, Question
 from django.utils import timezone
 from visualizer.views import Estadisticas
-
+import datetime
 
 class VisualizerTest(BaseTestCase):
 
-    def test1(self):
+    def test_num_votaciones_totales(self):
         pregunta = Question(desc='pregunta1', )
         pregunta.save()
         votacion = Voting(pk=1000,
-                             desc='descripcionVotacion1',
-                             name='votacion1',
+                             desc='descripcionVotacion0',
+                             name='votacion0',
                              question = pregunta,
-                             start_date=timezone.now(),
+                             start_date=timezone.now() + datetime.timedelta(days=1),
                              )
+        votacion = Voting(pk=1001,
+                          desc='descripcionVotacion1',
+                          name='votacion1',
+                          question=pregunta,
+                          start_date=timezone.now(),
+                          end_date=timezone.now() + datetime.timedelta(month=2),
+                          )
+        votacion = Voting(pk=1002,
+                          desc='descripcionVotacion2',
+                          name='votacion2',
+                          question=pregunta,
+                          start_date=timezone.now(),
+                          end_date=timezone.now() + datetime.timedelta(month=5),
+                          )
+        votacion = Voting(pk=1003,
+                          desc='descripcionVotacion3',
+                          name='votacion3',
+                          question=pregunta,
+                          start_date=timezone.now(),
+                          end_date=timezone.now() + datetime.timedelta(month=2),
+                          )
+        votacion = Voting(pk=1004,
+                          desc='descripcionVotacion3',
+                          name='votacion4',
+                          question=pregunta,
+                          start_date=timezone.now(),
+                          end_date=timezone.now() + datetime.timedelta(month=8),
+                          )
         votacion.save()
 
 
 
         resultados = Estadisticas.getEstadisticas(self)
-        self.assertTrue(resultados[0] ==1)
+        self.assertTrue(resultados[0] ==5)
+
+    def test_num_votaciones_sinEmpezar(self):
+        pregunta = Question(desc='pregunta1', )
+        pregunta.save()
+        votacion = Voting(pk=1001,
+                             desc='descripcionVotacion1',
+                             name='votacion1',
+                             question = pregunta,
+                             start_date=timezone.now() ,
+                             end_date=timezone.now()+ datetime.timedelta(month=2),
+                             )
+        votacion = Voting(pk=1002,
+                          desc='descripcionVotacion2',
+                          name='votacion2',
+                          question=pregunta,
+                          start_date=timezone.now(),
+                          end_date=timezone.now() + datetime.timedelta(month=5),
+                          )
+        votacion = Voting(pk=1003,
+                          desc='descripcionVotacion3',
+                          name='votacion3',
+                          question=pregunta,
+                          start_date=timezone.now(),
+                          end_date=timezone.now() + datetime.timedelta(month=2),
+                          )
+        votacion = Voting(pk=1004,
+                          desc='descripcionVotacion3',
+                          name='votacion4',
+                          question=pregunta,
+                          start_date=timezone.now(),
+                          end_date=timezone.now() + datetime.timedelta(month=8),
+                          )
+        votacion.save()
+
+
+
+        resultados = Estadisticas.getEstadisticas(self)
+        self.assertTrue(resultados[0] ==5)
 
 
