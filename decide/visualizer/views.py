a9f45ad4134d051c03a607b4b195a2fca1d53cd8
marjimveg1 <maria-2696@hotmail.com>
2019-01-28 11:00:20 +0100

    Añadido estadistica votaciones mensuales #10

--- decide/visualizer/views.py
+++ decide/visualizer/views.py
@@ -95,8 +95,21 @@ class DashboardEstadisticas(TemplateView):
             context['numVotacionesSinEmpezar'] = estadisticas[1]
             context['numVotacionesActivas'] = estadisticas[2]
             context['mediaOpciones'] =estadisticas[3]
-            context['tantoPorCienVotacionesCerradas'] = estadisticas[4]
-            context['todosVotos'] = estadisticas[5]
+            context['todosVotos'] = estadisticas[4]
+
+            #Añadiendo estadísticas de votaciones por mes
+            context['votacionesEnero'] = estadisticas[5]
+            context['votacionesFebrero'] = estadisticas[6]
+            context['votacionesMarzo'] = estadisticas[7]
+            context['votacionesAbril'] = estadisticas[8]
+            context['votacionesMayo'] = estadisticas[9]
+            context['votacionesJunio'] = estadisticas[10]
+            context['votacionesJulio'] = estadisticas[11]
+            context['votacionesAgosto'] = estadisticas[12]
+            context['votacionesSeptiembre'] = estadisticas[13]
+            context['votacionesOctubre'] = estadisticas[14]
+            context['votacionesNoviembre'] = estadisticas[15]
+            context['votacionesDiciembre'] = estadisticas[16]
 
         except:
             raise Http404
@@ -115,24 +128,50 @@ class Estadisticas():
 
         suma = 0
         for votacion in Voting.objects.all():
-            a = votacion
             if (votacion.question.options.none):
                 break
             else:
                 for opcion in votacion.question.options.option:
                     suma += 1
 
+        votacionesEnero = Voting.objects.filter(start_date__month=1)
+        votacionesFebrero = Voting.objects.filter(start_date__month=2)
+        votacionesMarzo = Voting.objects.filter(start_date__month=3)
+        votacionesAbril = Voting.objects.filter(start_date__month=4)
+        votacionesMayo = Voting.objects.filter(start_date__month=5)
+        votacionesJunio = Voting.objects.filter(start_date__month=6)
+        votacionesJulio = Voting.objects.filter(start_date__month=7)
+        votacionesAgosto = Voting.objects.filter(start_date__month=8)
+        votacionesSeptiembre = Voting.objects.filter(start_date__month=9)
+        votacionesOctubre = Voting.objects.filter(start_date__month=10)
+        votacionesNoviembre = Voting.objects.filter(start_date__month=11)
+        votacionesDiciembre = Voting.objects.filter(start_date__month=12)
+
+
+
         mediaOpcionesPorVotacion = suma / numVotacionesTotales
 
         todosVotos = Census.objects.all().count()
 
-        estadisticas.append(numVotacionesTotales) #0
-        print(numVotacionesTotales)
+        estadisticas.append(numVotacionesTotales)  #0
         estadisticas.append(numVotacionesSinEmpezar) #1
         estadisticas.append(numVotacionesActivas) #2
         estadisticas.append(mediaOpcionesPorVotacion) #3
-        estadisticas.append(todosVotos)  # 5
-        estadisticas.append(todosVotos)  # 6
+        estadisticas.append(todosVotos)  # 4
+
+        #Añadiendo estadísticas votaciones por meses
+        estadisticas.append(votacionesEnero) #5
+        estadisticas.append(votacionesFebrero) #6
+        estadisticas.append(votacionesMarzo) #7
+        estadisticas.append(votacionesAbril) #8
+        estadisticas.append(votacionesMayo) #9
+        estadisticas.append(votacionesJunio) #10
+        estadisticas.append(votacionesJulio) #11
+        estadisticas.append(votacionesAgosto) #12
+        estadisticas.append(votacionesSeptiembre) #13
+        estadisticas.append(votacionesOctubre) #14
+        estadisticas.append(votacionesNoviembre) #15
+        estadisticas.append(votacionesDiciembre) #16
 
         return estadisticas
 
